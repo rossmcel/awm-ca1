e1aee19c64a30e4350d7feb0ae396f088c2182c0
Ross McElhinney <rossmcelhinney123@gmail.com>
2022-11-07 18:09:43 +0000

    Add Functionality for Adding Current Location to DB

--- lab4_ross/static/map.js
+++ lab4_ross/static/map.js
@@ -2,13 +2,33 @@ const copy =
     "© <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors";
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const osm = L.tileLayer(url, { attribution: copy });
-const map = L.map("map", { layers: [osm], minZoom: 5 });
+const map = L.map("map", { layers: [osm], minZoom: 2 });
 
 const marker = L.marker();
 
-map.locate()
-    .on("locationfound", (e) => map.setView(e.latlng, 8))
-    .on("locationerror", () => map.setView([0, 0], 5));
+// map.locate()
+//     .on("locationfound", (e) => map.setView(e.latlng, 8))
+//     .on("locationerror", () => map.setView([0, 0], 5));
+
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    const radius = e.accuracy;
+    const currentLocationMarker = L.marker();
+    currentLocationMarker
+    .setLatLng(e.latlng)
+    .addTo(map)
+    .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    const markerNameElement = document.getElementById("id_name");
+    const latElement = document.getElementById("id_lat");
+    const lngElement = document.getElementById("id_lng");
+    markerNameElement.value = "Current Location";
+    latElement.value = currentLocationMarker._latlng.lat;
+    lngElement.value = currentLocationMarker._latlng.lng;
+}
+
+map.on('locationfound', onLocationFound);
    
 map.on('click', function (e) {
     console.log("Map Clicked");
@@ -18,8 +38,10 @@ map.on('click', function (e) {
     console.log(marker);
     //document.getElementById("selectedLocationLat").innerHTML = marker._latlng.lat;
     //document.getElementById("selectedLocationLng").innerHTML = marker._latlng.lng;
+    const markerNameElement = document.getElementById("id_name");
     const latElement = document.getElementById("id_lat");
     const lngElement = document.getElementById("id_lng");
+    markerNameElement.value = "Selected Point";
     latElement.value = marker._latlng.lat;
     lngElement.value = marker._latlng.lng;
 });
@@ -41,3 +63,26 @@ async function render_markers() {
 }
 
 map.on("moveend", render_markers);
+
+
+
+// var map = L.map('map').fitWorld();
+
+// L.tileLayer('https://tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png', {
+//     maxZoom: 19,
+//     attribution: '© OpenStreetMap'
+// }).addTo(map);
+
+// map.locate({setView: true, maxZoom: 16});
+
+
+// function onLocationFound(e) {
+//     var radius = e.accuracy;
+
+//     L.marker(e.latlng).addTo(map)
+//         .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+//     L.circle(e.latlng, radius).addTo(map);
+// }
+
+// map.on('locationfound', onLocationFound);
